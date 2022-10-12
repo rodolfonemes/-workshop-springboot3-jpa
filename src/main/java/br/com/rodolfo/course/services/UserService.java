f4c02a7a0f86946d599055b0e80b8aa2d31faf75
Rodolfo Nemes <tecnico.rodolfoo@gmail.com>
2022-10-12 20:35:50 -0300

    Exception handling - update

--- src/main/java/br/com/rodolfo/course/services/UserService.java
+++ src/main/java/br/com/rodolfo/course/services/UserService.java
@@ -12,6 +12,7 @@ import br.com.rodolfo.course.entities.User;
 import br.com.rodolfo.course.repositories.UserRepository;
 import br.com.rodolfo.course.services.exceptions.DatabaseException;
 import br.com.rodolfo.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -46,9 +47,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
